71ae0c9f92707d7ced278cec81b97194a256c1ab
andyrouge81 <andyrouge81@gmail.com>
2025-12-26 09:10:19 +0100

    feat(dto): adding dto files

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruitapih2/model/dto/FruitCreateRequest.java
@@ -0,0 +1,24 @@
+package cat.itacademy.s04.t02.n01.fruitapih2.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FruitCreateRequest {
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    @NotNull(message = "Weight must not be null ")
+    @Positive(message = "Weight must no be negative")
+    private Integer weightInKilos;
+
+
+
+}
