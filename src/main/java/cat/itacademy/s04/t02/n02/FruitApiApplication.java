83c9659eb790020ae953f0d493de79cd7ddb7446
andyrouge81 <andyrouge81@gmail.com>
2026-01-03 12:08:21 +0100

    feat: add fruit API application

--- src/main/java/cat/itacademy/s04/t02/n02/FruitApiApplication.java
+++ src/main/java/cat/itacademy/s04/t02/n02/FruitApiApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FruitApiH2Application {
+public class FruitApiApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(FruitApiH2Application.class, args);
+        SpringApplication.run(FruitApiApplication.class, args);
     }
 
 }
