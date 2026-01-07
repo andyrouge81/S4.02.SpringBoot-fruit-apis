1125730d8a5c9b0bddb516ee86a160498910ac28
andyrouge81 <andyrouge81@gmail.com>
2026-01-07 18:13:46 +0100

    feat(order): add repository

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/repository/OrderRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/repository/OrderRepository.java
@@ -1,4 +1,10 @@
 package cat.itacademy.s04.t02.n03.fruit.repository;
 
-public class OrderRepository {
+import cat.itacademy.s04.t02.n03.fruit.domain.document.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OrderRepository extends MongoRepository<Order, String> {
+
+    //boolean existsById(String id);
+
 }
