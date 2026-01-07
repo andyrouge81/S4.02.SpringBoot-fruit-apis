ddf07a6b2426489b9d69602d456fda3756719a28
andyrouge81 <andyrouge81@gmail.com>
2026-01-07 18:21:19 +0100

    docker: add dockerfile and docker-compose files

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/FruitOrderApplication.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/FruitOrderApplication.java
@@ -1,14 +1,14 @@
-package cat.itacademy.s04.t02.n02;
+package cat.itacademy.s04.t02.n03.fruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FruitApiApplication {
+public class FruitOrderApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(FruitApiApplication.class, args);
+        SpringApplication.run(FruitOrderApplication.class, args);
     }
 
 }
