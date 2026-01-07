4f21ad30a15e44be235c980c12e9054c5e847b6f
andyrouge81 <andyrouge81@gmail.com>
2026-01-07 18:19:08 +0100

    feat(order): add controller

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
@@ -1,4 +1,61 @@
 package cat.itacademy.s04.t02.n03.fruit.controller;
 
+import cat.itacademy.s04.t02.n03.fruit.dto.request.OrderCreateRequest;
+import cat.itacademy.s04.t02.n03.fruit.dto.request.OrderUpdateRequest;
+import cat.itacademy.s04.t02.n03.fruit.dto.response.OrderResponse;
+import cat.itacademy.s04.t02.n03.fruit.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService){
+        this.orderService= orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> create(@Valid @RequestBody OrderCreateRequest request){
+
+        OrderResponse response = orderService.create(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAllOrders(){
+
+        return ResponseEntity.ok(orderService.findAllOrders());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> findById(@PathVariable String id) {
+
+        return ResponseEntity.ok(orderService.findById(id));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderResponse> update(@PathVariable String id, @Valid @RequestBody OrderUpdateRequest request){
+
+        return ResponseEntity.ok(orderService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id){
+
+        orderService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
