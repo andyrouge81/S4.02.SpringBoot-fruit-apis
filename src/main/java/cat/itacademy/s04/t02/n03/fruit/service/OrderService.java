f63861954ed5a734f1afbd0d3a50ba54d12c51f9
andyrouge81 <andyrouge81@gmail.com>
2026-01-07 18:14:35 +0100

    feat(order): add OrderService interface

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/service/OrderService.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/service/OrderService.java
@@ -1,4 +1,20 @@
 package cat.itacademy.s04.t02.n03.fruit.service;
 
-public class OrderService {
+import cat.itacademy.s04.t02.n03.fruit.dto.request.OrderCreateRequest;
+import cat.itacademy.s04.t02.n03.fruit.dto.request.OrderUpdateRequest;
+import cat.itacademy.s04.t02.n03.fruit.dto.response.OrderResponse;
+
+
+import java.util.List;
+
+public interface OrderService {
+
+    OrderResponse create(OrderCreateRequest request);
+    List<OrderResponse> findAllOrders();
+    OrderResponse findById(String id);
+    OrderResponse update(String id, OrderUpdateRequest request);
+    void deleteById(String id);
+
 }
+
+
