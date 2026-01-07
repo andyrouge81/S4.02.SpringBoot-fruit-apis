b5de62feab09a1f116aa62385ecf782debe585da
andyrouge81 <andyrouge81@gmail.com>
2026-01-07 18:07:51 +0100

    feat(order): add document Order

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/domain/document/Order.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/domain/document/Order.java
@@ -1,4 +1,38 @@
 package cat.itacademy.s04.t02.n03.fruit.domain.document;
 
+import cat.itacademy.s04.t02.n03.fruit.domain.embedded.OrderItem;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "orders")
 public class Order {
+
+    @Id
+    private String id;
+
+    private String clientName;
+
+    private LocalDate deliveryDate;
+
+    private List<OrderItem> items;
+
+    public Order(String clientName, LocalDate deliveryDate, List<OrderItem> items){
+        this.clientName = clientName;
+        this.deliveryDate = deliveryDate;
+        this.items = items;
+    }
+
+
 }
